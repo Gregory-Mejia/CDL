f861a730ecd1c5841c483b6229c9a95790c18763
therealDDR <ddr01953@gmail.com>
2026-03-01 21:40:34 -0800

    Update and rename ESPresence_logic.py to esplogic.py
    renamed file to esplogic.py
    changed how the code parses mqtt topics

--- esplogic.py
+++ esplogic.py
@@ -4,24 +4,27 @@ import datetime
 
 mqttbroker = "127.0.0.1"
 jsonf = 'students.json'
-escapedist = 4.5 # distance to door in meters
+escapedist = 3 # distance to door in meters
 self_id = "iBeacon:e5ca1ade-f007-ba11-0000-000000000000-148-53065"
 present_students = set()
 
 def on_message(client, userdata, msg):
-    device_id = msg.topic.split('/')[-1]
+    device_id = msg.topic.split('/')[-2]
     # prevents self-detection loop
     if device_id == self_id:
         return
     try:
         # checks database for known students
-        with open(jsonf, 'r') as f: db = json.load(f)
+        with open(jsonf, 'r') as f:
+            db = json.load(f)
         
         if device_id in db:
+            # parses mqtt topic for device id
             student_name = db[device_id]
             data = json.loads(msg.payload.decode())
             dist = data.get("distance", 99)
-
+            
+            # logic for distance
             if dist < escapedist:
                 if student_name not in present_students:
                     print(f"{student_name} entered the room.")
@@ -31,14 +34,15 @@ def on_message(client, userdata, msg):
                     timestamp = datetime.datetime.now().strftime("%H:%M:%S")
                     print(f"{student_name} left at {timestamp}!")
                     present_students.remove(student_name)
-    except: pass
-
+    except Exception as e:
+        print("Error in on_message:", e)
+        
 client = mqtt.Client()
 client.on_message = on_message
 
 print(f"Connecting to broker at {mqttbroker}...")
 client.connect(mqttbroker, 1883)
-client.subscribe("espresense/devices/#")
+client.subscribe("espresense/devices/+/94cf49")
 
 print("Monitoring presense (Ctrl+C to stop).")
 client.loop_forever()
